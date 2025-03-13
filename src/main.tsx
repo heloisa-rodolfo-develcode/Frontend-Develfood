2c330f5a4c9874762d4ce75660aac86bed7b738d
heloisa-rodolfo-develcode <heloisa.rodolfo@develcode.com.br>
2025-03-13 19:52:45 -0300

    removing context

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,10 @@ import { createRoot } from "react-dom/client";
 
 import './global.css';
 import { App } from "./App";
-import { RestaurantProvider } from "./pages/profile/context/restaurantContext";
+
 
 createRoot(document.getElementById("root")!).render(
-  <RestaurantProvider>
   <StrictMode>
     <App /> 
   </StrictMode>
-  </RestaurantProvider>
 );
